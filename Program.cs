b6371d03b360c2bf4fe0a645783cdf92c3b6e46b
David Belisle <david1albertogmail.com>
2021-12-17 12:53:53 -0600

    Prueba de amplifier probada

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Amplifier;
+using System;
 
 namespace OpenCL_Test
 {
@@ -6,7 +7,42 @@ namespace OpenCL_Test
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
+            var compiler = new OpenCLCompiler();
+            Console.WriteLine("\nList Devices----");
+            foreach (var item in compiler.Devices)
+            {
+                Console.WriteLine(item);
+            }
+
+            compiler.UseDevice(0);
+            compiler.CompileKernel(typeof(SimpleKernels));
+
+            Console.WriteLine("\nList Kernels----");
+            foreach (var item in compiler.Kernels)
+            {
+                Console.WriteLine(item);
+            }
+
+            //Create variable x, y with sample data
+            Array x = new float[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Array y = new float[9];
+            //Get the execution engine
+            var exec = compiler.GetExec();
+            //Execute fill kernel method to fill the y with constant value 0.5
+            exec.Fill(y, 0.5f);
+            //Execute SAXPY kernel method
+            exec.SAXPY(x, y, 2f);
+
+            exec.AddData(x, x, x);
+            //Print the result
+            Console.WriteLine("\nResult----");
+            for (int i = 0; i < y.Length; i++)
+            {
+                Console.Write(y.GetValue(i) + " ");
+            }
         }
+
+       
     }
 }
---
+++ SimpleKernels.cs
@@ -0,0 +1,34 @@
+﻿using Amplifier.OpenCL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCL_Test
+{
+    class SimpleKernels : OpenCLFunctions
+    {
+        [OpenCLKernel]
+        void AddData([Global] float[] a, [Global] float[] b, [Global] float[] r)
+        {
+            int i = get_global_id(0);
+            b[i] = 0.5f * b[i];
+            r[i] = a[i] + b[i];
+        }
+        [OpenCLKernel]
+        void Fill([Global] float[] x, float value)
+        {
+            int i = get_global_id(0);
+
+            x[i] = value;
+        }
+        [OpenCLKernel]
+        void SAXPY([Global] float[] x, [Global] float[] y, float a)
+        {
+            int i = get_global_id(0);
+            y[i] += a * x[i];
+            
+        }
+    }
+}
